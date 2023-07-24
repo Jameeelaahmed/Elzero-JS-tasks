4da6bc990b9ee322f6030ab154396436d9482c59
Jameeelaahmed <gamilas320@gmail.com>
2023-07-24 05:45:00 +0300

    update

---
+++ Chalenges/chalenge5 (vid 37)/chalenge5.js
@@ -0,0 +1,59 @@
+//! 1
+
+//* write with ternary if syntax
+// let a =-50;
+// if(a<10){
+//     console.log(10);
+// } else if (a>=10 && a<=40){
+//     console.log("10 to 40");
+// } else if(a>40){
+//     console.log("> 40");
+// } else {
+//     console.log("UNKNOWN");
+// }
+
+//* solution 
+
+// a==10? console.log("10"): a>=10 && a<= 40? console.log("10 to 40"): a>40 ? console.log("> 40"): console.log("UNKNOWN");
+
+
+let st="Elzero Web School";
+
+//! 1
+
+//* write with ternary if syntax
+
+// if((st.length * 2).toString() === "34"){
+//     console.log("Good");
+// }
+
+//! 2 
+
+//* w position may change 
+
+// if(st.charAt(st.indexOf("w")).toLowerCase()!== "w") {
+//     console.log("GOOD");
+// }
+
+//! 3
+
+// if (st.length !== "string")
+// {
+//     console.log("Good")
+// }
+
+//! 4
+
+// if (typeof st.length === "number")
+// {
+//     console.log("Good");
+// }
+
+//! 5
+
+// if (st.substring(0,6).repeat(2) === "ElzeroElzero")
+// {
+//     console.log("Good")
+// }
+
+
